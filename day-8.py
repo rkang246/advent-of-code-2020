f55c344e1501a63af1330d12028b23fd770a4624
rqkang <rqkang@umich.edu>
2020-12-24 14:13:36 -0500

    completed day-8 part b

--- day-8.py
+++ day-8.py
@@ -1,16 +1,36 @@
+def simulate(instr):
+    acc = 0
+    pc = 0
+    visited = [False] * len(instr)
+    while not visited[pc]:
+        if len(instr[pc]) == 0:
+            return (acc, True)
+        visited[pc] = True
+        if instr[pc].split()[0] == "acc":
+            acc += int(instr[pc].split()[1])
+
+        if instr[pc].split()[0] == "jmp":
+            incr = int(instr[pc].split()[1])
+            pc += incr
+        else:
+            pc += 1
+    return (acc, False)
+
+instr = open("day-8.in", "r").read().split("\n")
+print(simulate(instr)[0])
+
+
 acc = 0
 pc = 0
-instr = open("day-8.in", "r").read().split("\n")
-visited = [False] * len(instr)
-while not visited[pc]:
-    visited[pc] = True
-    if instr[pc].split()[0] == "acc":
-        acc += int(instr[pc].split()[1])
+for change in range(0, len(instr)):
+    origLine = instr[change]
+    if instr[change].split()[0] == "nop":
+        instr[change] = "jmp" + " " + instr[change].split()[1]
+    elif instr[change].split()[0] == "jmp":
+        instr[change] = "nop" + " " + instr[change].split()[1]
+    simRes = simulate(instr)
+    if simRes[1]:
+        print(simRes[0])
+        break
+    instr[change] = origLine
 
-    if instr[pc].split()[0] == "jmp":
-        pc += int(instr[pc].split()[1])
-    else:
-        pc += 1
-print(acc)
-
-#todo part 2
